2b4d9c974e5fb38aee0a1dd5a5c6b360f336becf
Ken Watford <kwatford@cise.ufl.edu>
2009-11-27 20:24:21 -0500

    A block was indented a bit too far, causing search of classes from MATLAB packages to be skipped

--- mltypes/__init__.py
+++ mltypes/__init__.py
@@ -12,12 +12,12 @@ def _findtype(typelist):
             adjusted_modstr = "mltypes.%s" % modstr
         else:
             adjusted_modstr = "mltypes"
-            try:
-                tempmod = __import__(adjusted_modstr, globals(), locals(), [clsstr], 0)
-                cls = getattr(tempmod, clsstr);
-                return cls;
-            except:
-                pass            
+        try:
+            tempmod = __import__(adjusted_modstr, globals(), locals(), [clsstr], 0)
+            cls = getattr(tempmod, clsstr);
+            return cls;
+        except:
+            pass            
     # if we got this far and found nothing, just use mx.Array
     return mx.Array;
 
