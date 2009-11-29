5ce0121f0b86749559234263bf289badfcc9c766
Ken Watford <kwatford@cise.ufl.edu>
2009-11-29 16:07:41 -0500

    Normalizing style in engmodule

--- engmodule.c
+++ engmodule.c
@@ -21,14 +21,12 @@ static PyModuleDef engmodule_def = {
 #endif
 
 #ifndef PyMODINIT_FUNC	/* declarations for DLL import/export */
-#define PyMODINIT_FUNC PyObject*
+#define PyMODINIT_FUNC PyObject *
 #endif
-PyMODINIT_FUNC
-initengmodule(void)
-{
-
+PyMODINIT_FUNC initengmodule(void) {
   #if PY_VERSION_HEX < PY3K_VERSION_HEX
-  PyObject* m = Py_InitModule3("matlab.eng", eng_methods, "MATLAB Engine library, for spawning MATLAB processes. (placeholder)");
+  PyObject* m = Py_InitModule3("matlab.eng", eng_methods, 
+			 "MATLAB Engine library, for spawning MATLAB processes. (placeholder)");
   if (!m) return;
   #else
   PyObject* m = PyModule_Create(&engmodule_def);
