a632d27d8f128ab74ec3c9e137ce2a487351bd6d
Ken Watford <kwatford@cise.ufl.edu>
2009-11-29 15:51:23 -0500

    Normalizing style in sharedfuncs

--- sharedfuncs.c
+++ sharedfuncs.c
@@ -36,20 +36,20 @@
 */
 /* 512 is probably a bit too generous. I believe MATLAB has a builtin limit - what is it? */
 #define MAX_MXTYPE_NAME_SIZE 512
-mxArray* box_by_type(PyObject* pyobj) {
+mxArray *box_by_type(PyObject *pyobj) {
   static char mlname[MAX_MXTYPE_NAME_SIZE] = {0};
-  static char* package = "py.types.%s.%s";
+  static char *package = "py.types.%s.%s";
   PYMEX_DEBUG("Trying to box %p\n", pyobj);
-  mxArray* box = NULL;
-  mxArray* mxname;
-  mxArray* which;
+  mxArray *box = NULL;
+  mxArray *mxname;
+  mxArray *which;
   int ret = -1;
   if (!pyobj) {
     ret = mexCallMATLAB(1,&box,0,NULL,PYMEX_MATLAB_VOIDPTR);
   }
   else {
-    PyObject* type = (PyObject*) pyobj->ob_type;
-    PyObject* mro;
+    PyObject *type = (PyObject*) pyobj->ob_type;
+    PyObject *mro;
     if (PyType_Check(pyobj)) {
       /* Calling type on a type gives us back type, and
 	 calling mro on type doesn't work quite the same,
@@ -67,7 +67,7 @@ mxArray* box_by_type(PyObject* pyobj) {
     Py_ssize_t len = PySequence_Length(mro);
     Py_ssize_t i;
     for (i=0; i<len && ret; i++) {
-      PyObject* item = PySequence_GetItem(mro, i);
+      PyObject *item = PySequence_GetItem(mro, i);
       if (item == pyobj) {
 	PYMEX_DEBUG("Pointers match!?\n"); 
       }
@@ -77,10 +77,11 @@ mxArray* box_by_type(PyObject* pyobj) {
       else {
 	PYMEX_DEBUG("Ok, getting name...\n");
       }
-      PyObject* modname = PyObject_GetAttrString(item, "__module__");
-      PyObject* cleanmodname = PyObject_CallMethod(modname, "strip", "s", "_");
-      PyObject* name = PyObject_GetAttrString(item, "__name__");
-      snprintf(mlname, MAX_MXTYPE_NAME_SIZE, package, PyBytes_AsString(cleanmodname), PyBytes_AsString(name));
+      PyObject *modname = PyObject_GetAttrString(item, "__module__");
+      PyObject *cleanmodname = PyObject_CallMethod(modname, "strip", "s", "_");
+      PyObject *name = PyObject_GetAttrString(item, "__name__");
+      snprintf(mlname, MAX_MXTYPE_NAME_SIZE, package, 
+	       PyBytes_AsString(cleanmodname), PyBytes_AsString(name));
       PYMEX_DEBUG("Checking for %s...\n", mlname);
       Py_DECREF(name);
       Py_DECREF(cleanmodname);
@@ -111,43 +112,43 @@ mxArray* box_by_type(PyObject* pyobj) {
 }
 
 /* boxes the object, stealing the reference */
-mxArray* box (PyObject* pyobj) {
-  mxArray* boxed = NULL;
+mxArray *box (PyObject *pyobj) {
+  mxArray *boxed = NULL;
   if (!pyobj) {
     PYMEX_DEBUG("Boxing null object.");
   }
   boxed = box_by_type(pyobj);
   if (!boxed) return NULL;
   if (pyobj) mexLock();
-  mxArray* ptr_field = mxGetProperty(boxed, 0, "pointer");
-  void** ptr = mxGetData(ptr_field);
+  mxArray *ptr_field = mxGetProperty(boxed, 0, "pointer");
+  void **ptr = mxGetData(ptr_field);
   *ptr = (void*) pyobj;
   mxSetProperty(boxed, 0, "pointer", ptr_field);
   return boxed;
 }
 
 /* Box a borrowed reference (increfs it first) */
-mxArray* boxb (PyObject* pyobj) {
+mxArray *boxb (PyObject *pyobj) {
   Py_XINCREF(pyobj);
   return box(pyobj);
 }
 
 /* Unboxes an object, returning a borrowed reference */
-PyObject* unbox (const mxArray* mxobj) {  
+PyObject *unbox (const mxArray *mxobj) {  
   if (!mxobj) return PyErr_Format(MATLABError, "Can't unbox from null pointer");
   if (mxIsPyNull(mxobj)) {
     PYMEX_DEBUG("Unboxed a null object.");
     return PyErr_Format(MATLABError, "Unboxed pointer is null.");
   }
   else {
-    void** ptr = mxGetData(mxGetProperty(mxobj, 0, "pointer"));
+    void **ptr = mxGetData(mxGetProperty(mxobj, 0, "pointer"));
     return (PyObject*) *ptr;
   }
 }
 
 /* Unboxes an object, returning a new reference */
-PyObject* unboxn (const mxArray* mxobj) {
-  PyObject* pyobj;
+PyObject *unboxn (const mxArray *mxobj) {
+  PyObject *pyobj;
   if (mxIsPyObject(mxobj)) {
     pyobj = unbox(mxobj);
     Py_XINCREF(pyobj);
@@ -162,17 +163,17 @@ PyObject* unboxn (const mxArray* mxobj) {
    Only pass it voidptr (and subclasses thereof), as
    it does not check this before doing the mxGetProperty
  */
-bool mxIsPyNull (const mxArray* mxobj) {
-  void** ptr = mxGetData(mxGetProperty(mxobj, 0, "pointer"));
+bool mxIsPyNull (const mxArray *mxobj) {
+  void **ptr = mxGetData(mxGetProperty(mxobj, 0, "pointer"));
   return !*ptr;
 }
 
 /* Asks the MATLAB interpreter if the object isa subclass of voidptr.
    This includes null pointers and things not descended from object.
  */
-bool mxIsPyObject(const mxArray* mxobj) {
-  mxArray* boolobj;
-  mxArray* args[2];
+bool mxIsPyObject(const mxArray *mxobj) {
+  mxArray *boolobj;
+  mxArray *args[2];
   args[0] = (mxArray*) mxobj;
   args[1] = mxCreateString(PYMEX_MATLAB_VOIDPTR);
   mexCallMATLAB(1,&boolobj,2,args,"isa");
@@ -180,31 +181,31 @@ bool mxIsPyObject(const mxArray* mxobj) {
   return mxIsLogicalScalarTrue(boolobj);
 }
 
-PyObject* mxChar_to_PyBytes(const mxArray* mxchar) {
+PyObject *mxChar_to_PyBytes(const mxArray *mxchar) {
   if (!mxchar || !mxIsChar(mxchar))
     mexErrMsgTxt("Input isn't a mxChar");
-  char* tempstring = mxArrayToString(mxchar);
+  char *tempstring = mxArrayToString(mxchar);
   if (!tempstring)
     mexErrMsgTxt("Couldn't stringify mxArray for some reason.");
-  PyObject* pystr = PyBytes_FromString(tempstring);
+  PyObject *pystr = PyBytes_FromString(tempstring);
   mxFree(tempstring);
   if (!pystr)
     mexErrMsgTxt("Couldn't convert from string to PyBytes");
   return pystr;
 }
 
-mxArray* PyBytes_to_mxChar(PyObject* pystr) {
+mxArray *PyBytes_to_mxChar(PyObject *pystr) {
   if (!pystr || !PyBytes_Check(pystr))
     mexErrMsgTxt("Input isn't a PyBytes");
-  char* tempstring = PyBytes_AsString( pystr);
-  mxArray* mxchar = mxCreateString(tempstring);
+  char *tempstring = PyBytes_AsString( pystr);
+  mxArray *mxchar = mxCreateString(tempstring);
   return mxchar;
 }
 
-mxArray* PyObject_to_mxChar(PyObject* pyobj) {
+mxArray *PyObject_to_mxChar(PyObject *pyobj) {
   if (pyobj) {
-    PyObject* pystr = PyObject_Str(pyobj);
-    mxArray* mxchar = PyBytes_to_mxChar(pystr);
+    PyObject *pystr = PyObject_Str(pyobj);
+    mxArray *mxchar = PyBytes_to_mxChar(pystr);
     Py_DECREF(pystr);
     return mxchar;
   } 
@@ -214,9 +215,9 @@ mxArray* PyObject_to_mxChar(PyObject* pyobj) {
   }
 }
 
-PyObject* mxCell_to_PyTuple(const mxArray* mxobj) {
+PyObject *mxCell_to_PyTuple(const mxArray *mxobj) {
   mwSize numel = mxGetNumberOfElements(mxobj);
-  PyObject* pyobj = PyTuple_New(numel);
+  PyObject *pyobj = PyTuple_New(numel);
   mwSize i;
   for (i=0; i<numel; i++) {
     PyTuple_SetItem(pyobj, i, Any_mxArray_to_PyObject(mxGetCell(mxobj, i)));
@@ -224,13 +225,13 @@ PyObject* mxCell_to_PyTuple(const mxArray* mxobj) {
   return pyobj;
 }
 
-PyObject* mxCell_to_PyTuple_recursive(const mxArray* mxobj) {
+PyObject *mxCell_to_PyTuple_recursive(const mxArray *mxobj) {
   mwSize numel = mxGetNumberOfElements(mxobj);
-  PyObject* pyobj = PyTuple_New(numel);
+  PyObject *pyobj = PyTuple_New(numel);
   mwSize i;
   for (i=0; i<numel; i++) {
-    mxArray* item = mxGetCell(mxobj, i);
-    PyObject* pyitem;
+    mxArray *item = mxGetCell(mxobj, i);
+    PyObject *pyitem;
     if (mxIsCell(item))
       pyitem = mxCell_to_PyTuple_recursive(item);
     else
@@ -240,10 +241,10 @@ PyObject* mxCell_to_PyTuple_recursive(const mxArray* mxobj) {
   return pyobj;
 }
 
-mxArray* PySequence_to_mxCell(PyObject* pyobj) {
-  PyObject* item;
+mxArray *PySequence_to_mxCell(PyObject *pyobj) {
+  PyObject *item;
   Py_ssize_t len = PySequence_Length(pyobj);
-  mxArray* mxcell = mxCreateCellMatrix(1,len);
+  mxArray *mxcell = mxCreateCellMatrix(1,len);
   PERSIST_ARRAY(mxcell);
   mwIndex i;
   for (i=0; i<len; i++) {
@@ -254,9 +255,9 @@ mxArray* PySequence_to_mxCell(PyObject* pyobj) {
   return mxcell;
 }
 
-PyObject* Any_mxArray_to_PyObject(const mxArray* mxobj) {
+PyObject *Any_mxArray_to_PyObject(const mxArray *mxobj) {
   if (mxIsPyObject(mxobj)) {
-    PyObject* pyobj = unbox(mxobj);
+    PyObject *pyobj = unbox(mxobj);
     Py_XINCREF(pyobj);
     return pyobj;
   }
@@ -268,20 +269,20 @@ PyObject* Any_mxArray_to_PyObject(const mxArray* mxobj) {
   }
 }
 
-mxArray* Any_PyObject_to_mxArray(PyObject* pyobj) {
+mxArray *Any_PyObject_to_mxArray(PyObject *pyobj) {
   if (!pyobj)
     return box(pyobj); /* Null pointer */
   if (Py_mxArray_Check(pyobj))
     return mxArrayPtr(pyobj);
   else {
-    PyObject* utils = PyImport_ImportModule("pymexutil");
-    PyObject* arg;
+    PyObject *utils = PyImport_ImportModule("pymexutil");
+    PyObject *arg;
     if (PyTuple_Check(pyobj)) arg = PyTuple_Pack(1, pyobj);
     else {
       arg = pyobj;
       Py_INCREF(pyobj);
     }
-    PyObject* unpyed = PyObject_CallMethod(utils, "unpy", "O", arg);
+    PyObject *unpyed = PyObject_CallMethod(utils, "unpy", "O", arg);
     Py_DECREF(arg);
     if (PyErr_Occurred()) {
       if (PyErr_ExceptionMatches(PyExc_NotImplementedError))
@@ -289,7 +290,7 @@ mxArray* Any_PyObject_to_mxArray(PyObject* pyobj) {
       else return NULL;
     }
     if (Py_mxArray_Check(unpyed)) {
-      mxArray* retval = mxDuplicateArray(mxArrayPtr(unpyed));
+      mxArray *retval = mxDuplicateArray(mxArrayPtr(unpyed));
       PERSIST_ARRAY(retval);
       Py_DECREF(unpyed);
       return retval;
@@ -303,17 +304,17 @@ mxArray* Any_PyObject_to_mxArray(PyObject* pyobj) {
    It could operate at low capability by using a hardcoded list for
    the builtins and assuming that anything else has _object as a super.
  */
-PyObject* Calculate_matlab_mro(mxArray* mxobj) {
-  mxArray* argin[3];
+PyObject *Calculate_matlab_mro(mxArray *mxobj) {
+  mxArray *argin[3];
   argin[0] = mxobj;
   argin[1] = mxCreateLogicalScalar(1); /* addvirtual=true */
   argin[2] = mxCreateLogicalScalar(1); /* autosplit=true */
-  mxArray* argout[1] = {NULL};
+  mxArray *argout[1] = {NULL};
   mexSetTrapFlag(1);
   int err = mexCallMATLAB(1, argout, 3, argin, "mro");
   if (err) return PyObject_CallMethod(mexmodule, "__raiselasterror", "()");
   else {
-    PyObject* retval = mxCell_to_PyTuple_recursive(argout[0]);
+    PyObject *retval = mxCell_to_PyTuple_recursive(argout[0]);
     mxDestroyArray(argout[0]);
     mxDestroyArray(argin[1]);
     mxDestroyArray(argin[2]);
@@ -324,7 +325,7 @@ PyObject* Calculate_matlab_mro(mxArray* mxobj) {
 /* Attempts to locate an appropriate subclass of mx.Array using the mltypes package.
    If for some reason this fails, mx.Array is returned instead.
  */
-PyObject* Find_mltype_for(mxArray* mxobj) {
+PyObject *Find_mltype_for(mxArray *mxobj) {
   #define GET_MX_ARRAY_CLASS PyObject_GetAttrString(mxmodule, "Array")
   PyObject *newclass, *mrolist, *mltypes, *findtype;
   newclass = mrolist = mltypes = findtype = NULL;
@@ -367,8 +368,8 @@ PyObject* Find_mltype_for(mxArray* mxobj) {
 }
 
 
-PyObject* Py_mxArray_New(mxArray* mxobj, bool duplicate) {
-  mxArray* copy;
+PyObject *Py_mxArray_New(mxArray *mxobj, bool duplicate) {
+  mxArray *copy;
   if (duplicate) {
     copy = mxDuplicateArray(mxobj);
     mexMakeArrayPersistent(copy);
@@ -376,13 +377,13 @@ PyObject* Py_mxArray_New(mxArray* mxobj, bool duplicate) {
   else {
     copy = mxobj;
   }
-  PyObject* mxptr = mxArrayPtr_New(copy);
-  PyObject* args = PyTuple_New(0);
-  PyObject* kwargs = PyDict_New();
+  PyObject *mxptr = mxArrayPtr_New(copy);
+  PyObject *args = PyTuple_New(0);
+  PyObject *kwargs = PyDict_New();
   PyDict_SetItemString(kwargs, "mxpointer", mxptr);
-  PyObject* arraycls = Find_mltype_for(copy);
+  PyObject *arraycls = Find_mltype_for(copy);
   /* TODO: There is probably a better way to do this... */
-  PyObject* ret = PyObject_Call(arraycls, args, kwargs);
+  PyObject *ret = PyObject_Call(arraycls, args, kwargs);
   Py_DECREF(args);
   Py_DECREF(kwargs);
   Py_DECREF(mxptr);
@@ -390,30 +391,30 @@ PyObject* Py_mxArray_New(mxArray* mxobj, bool duplicate) {
   return ret;
 }
 
-int Py_mxArray_Check(PyObject* pyobj) {
+int Py_mxArray_Check(PyObject *pyobj) {
   return PyObject_IsInstance(pyobj, PyObject_GetAttrString(mxmodule, "Array"));
 }
 
-mxArray* PyObject_to_mxDouble(PyObject* pyobj) {
+mxArray *PyObject_to_mxDouble(PyObject *pyobj) {
   double val = PyFloat_AsDouble(pyobj);
   if (PyErr_Occurred()) return NULL;
-  mxArray* mxval = mxCreateDoubleScalar(val);
+  mxArray *mxval = mxCreateDoubleScalar(val);
   PERSIST_ARRAY(mxval);
   return mxval;
 }
 
-mxArray* PyObject_to_mxLong(PyObject* pyobj) {
+mxArray *PyObject_to_mxLong(PyObject *pyobj) {
   long val = PyLong_AsLong(pyobj);
   if (PyErr_Occurred()) return NULL;
   /* there is no mxCreateNumericScalar */
-  mxArray* mxval = mxCreateNumericMatrix(1, 1, mxINT64_CLASS, mxREAL);
+  mxArray *mxval = mxCreateNumericMatrix(1, 1, mxINT64_CLASS, mxREAL);
   PERSIST_ARRAY(mxval);
   long* ptr = (long*) mxGetData(mxval);
   ptr[0] = val;
   return mxval;
 }
 
-mxArray* PyObject_to_mxLogical(PyObject* pyobj) {
+mxArray *PyObject_to_mxLogical(PyObject *pyobj) {
   return mxCreateLogicalScalar(PyObject_IsTrue(pyobj));
 }
 
@@ -435,8 +436,8 @@ char mxClassID_to_Numpy_Typekind(mxClassID mxclass) {
   }
 }
 
-mxArray* mxArrayPtr(PyObject* pyobj) {
-  PyObject* ptr;
+mxArray *mxArrayPtr(PyObject *pyobj) {
+  PyObject *ptr;
   if (PyCObject_Check(pyobj)) {
     ptr = pyobj;
   }
@@ -451,12 +452,12 @@ mxArray* mxArrayPtr(PyObject* pyobj) {
   return (mxArray*) PyCObject_AsVoidPtr(ptr);
 }
 
-static void _mxArrayPtr_destructor(void* mxobj, void* desc) {
+static void _mxArrayPtr_destructor(void *mxobj, void *desc) {
   Py_XDECREF((PyObject*) desc);  
   if (mxobj) mxDestroyArray((mxArray*) mxobj);
 }
 
-PyObject* mxArrayPtr_New(mxArray* mxobj) {
+PyObject *mxArrayPtr_New(mxArray *mxobj) {
   if (!mxmodule)
     return PyErr_Format(PyExc_RuntimeError, "mxmodule not yet initialized");
   PERSIST_ARRAY(mxobj);
@@ -464,6 +465,6 @@ PyObject* mxArrayPtr_New(mxArray* mxobj) {
   return PyCObject_FromVoidPtrAndDesc(mxobj, mxmodule, _mxArrayPtr_destructor);
 }
 
-int mxArrayPtr_Check(PyObject* obj) {
+int mxArrayPtr_Check(PyObject *obj) {
   return (obj && PyCObject_Check(obj) && PyCObject_GetDesc(obj) == mxmodule);
 }
