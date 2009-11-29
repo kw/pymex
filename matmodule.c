0a62099b8454efe2679ac2d91d37942ae7ab7a6a
Ken Watford <kwatford@cise.ufl.edu>
2009-11-29 16:07:01 -0500

    Normalizing style in matmodule

--- matmodule.c
+++ matmodule.c
@@ -23,15 +23,13 @@ static PyModuleDef matmodule_def = {
 #ifndef PyMODINIT_FUNC	/* declarations for DLL import/export */
 #define PyMODINIT_FUNC PyObject*
 #endif
-PyMODINIT_FUNC
-initmatmodule(void)
-{
-
+PyMODINIT_FUNC initmatmodule(void) {
   #if PY_VERSION_HEX < PY3K_VERSION_HEX
-  PyObject* m = Py_InitModule3("matlab.mat", mat_methods, "MATLAB MAT-file interface (placeholder)");
+  PyObject *m = Py_InitModule3("matlab.mat", mat_methods, 
+			       "MATLAB MAT-file interface (placeholder)");
   if (!m) return;
   #else
-  PyObject* m = PyModule_Create(&matmodule_def);
+  PyObject *m = PyModule_Create(&matmodule_def);
   if (!m) return NULL;
   #endif
 
